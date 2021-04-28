654c9c45d2cc8a02cb9d1d7b408fa7b9f5ee4821
Tomasz Kasa kasowicz <tomasz.kasowicz@motorolasolutions.com>
2021-04-28 19:32:35 +0200

    test: add UT for transaction details component

--- src/app/components/transaction-details/transaction-details.component.spec.ts
+++ src/app/components/transaction-details/transaction-details.component.spec.ts
@@ -1,25 +1,46 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { WindowHeaderComponent } from '../window-header/window-header.component';
-
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
 import { TransactionDetailsComponent } from './transaction-details.component';
+import { Transaction } from '../../model/transaction';
 
 describe('TransactionDetailsComponent', () => {
-  let component: TransactionDetailsComponent;
-  let fixture: ComponentFixture<TransactionDetailsComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [TransactionDetailsComponent, WindowHeaderComponent]
-    }).compileComponents();
-  }));
+  let spectator: Spectator<TransactionDetailsComponent>;
+  const createComponent = createComponentFactory(TransactionDetailsComponent);
+  const transaction: Transaction = {
+    amount: 123.45,
+    categoryCode: '#123456',
+    merchant: 'merchant',
+    transactionDate: new Date(2021, 3, 27).getTime(),
+    transactionType: 'Transaction',
+    merchantLogo: 'Logo'
+  };
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TransactionDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createComponent();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator.component).toBeTruthy();
+  });
+
+  it('Should Render Details with logo', () => {
+    spectator.setInput({ transaction });
+    spectator.detectChanges();
+    expect(spectator.fixture).toMatchSnapshot();
+
+    const divElements = spectator.queryAll('.item');
+    expect(divElements.length).toEqual(4);
+
+    expect(spectator.query('img')).toBeTruthy();
+  });
+
+  it('Should Render Details without logo', () => {
+    const transactionWithoutLogo = { ...transaction, merchantLogo: undefined };
+    spectator.setInput({ transaction: transactionWithoutLogo });
+    spectator.detectChanges();
+    expect(spectator.fixture).toMatchSnapshot();
+
+    const divElements = spectator.queryAll('.item');
+    expect(divElements.length).toEqual(4);
+    expect(spectator.query('img')).toBeFalsy();
   });
 });
